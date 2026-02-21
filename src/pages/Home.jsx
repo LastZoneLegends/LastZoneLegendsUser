20b991d63cd8a277ecbcdaa87bdcfd1a0c17c747
LastZoneLegends <lastzonelegends0@gmail.com>
2026-02-21 08:36:35 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -133,13 +133,16 @@ export default function Home() {
             </Link>
           ))}
         </div>
-
+        
+    {/* My Contests */}
+        
     <section className="animate-fade-in">
-          <div className="flex items-center justify-between mb-3">
-            <h2 className="text-lg font-semibold text-white flex items-center gap-2">
+      <h2 className="text-lg font-semibold text-white flex items-center gap-2">
               <Calendar className="w-5 h-5 text-yellow-400" />
               My Contests
             </h2>
+          <div className=""grid grid-cols-3 gap-4 mb-3 mt-6">
+            
     
             {/* Upcoming */}
     <div
