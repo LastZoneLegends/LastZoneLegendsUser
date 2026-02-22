c861c9d82c0df70cd8e9de46bc9380ad1628f5e3
LastZoneLegends <lastzonelegends0@gmail.com>
2026-02-23 00:26:02 +0530

    Update Splash.jsx

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,18 +1,6 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import splashImage from "../assets/Splash.png";
 
 export default function Splash() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      navigate("/login"); // login page ka route
-    }, 2500);
-
-    return () => clearTimeout(timer);
-  }, [navigate]);
-
   return (
     <div className="w-screen h-screen">
       <img
